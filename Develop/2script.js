eeff797f80e34ddf3d63db937f44b26a1cecb6bc
Kimberly Park <quorrbbeeflash@yahoo.com>
2021-09-30 20:15:26 -0600

    password length

---
+++ Develop/2script.js
@@ -0,0 +1,7 @@
+// generatorfunctions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLower());
--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,50 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
-//characters that the password can contain
-var numeric = '1234567890';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lowerCase = 'abcdefghijklnopqrstuvwxyz';
-var special = '!#$%^&*()';
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// ask user about password length
+var characterLength = prompt("How many characters would you like your password to contain? Please enter a number betwixt (and including) 8 - 128");
+if (characterLength < 8) {
+        alert("Password must be betwixt 8 - 128 characters");
+}
+if (characterLength > 128) {
+    alert("Password must be betwixt 8 - 128 characters");
+}
 
 
 
 
 
+
+
+
+
+
+// generatorfunctions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+
+
+
+console.log(getRandomSymbol());
