76826431bf337cf72e6abb58ae35e229feac254d
Kimberly Park <quorrbbeeflash@yahoo.com>
2021-10-11 21:00:12 -0600

    customized it for my sister, Ramie

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,92 @@
-// Assignment code here
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
+function generatePassword() {
+
+ 
+  // Below is a list of all password Criteria:
+  
+  var pswLength = 0
+  lowerCase = false
+  upperCase = false
+  numericCharacters = false
+  specialCharacters = false
+
+// Welcome message to user
+alert("Welcome Ramie! You came to the right place to generate a secure random password. Well, let's get started :) ");
+
+// ===== while statement: will continue to run as long as condition is true. User enters the length of psw (must be between 8-128 characters )======
+
+while(pswLength > 128 || pswLength < 8){
+  pswLength = prompt("How long would you like  your passwored to be? Please enter a number betwixt 8 - 128")
+  if(pswLength > 128 || pswLength < 8){
+// if password does not fall within these parameters - alert is triggererd  (at least 8 characters and no more than 128 characters) 
+    alert("Please enter a number betwixt 8 and 128!")
+   
+  }
+}
+
+var criteriaList = []
+
+while(lowerCase===false && upperCase===false && numericCharacters===false && specialCharacters===false){
+
+ // ALERT - Ask if user wants Password WITH Lowercase 
+  var lowerCase = confirm("Would you like your password to have lowercase characters?")
+
+// User selected "OK" to lowercase character in psw...
+  if(lowerCase){
+
+    criteriaList.push('abcdefghijklmnopqrstuvwxyz'.split(''))
+  }
+
+ // ALERT - Ask if user want password with Uppercase
+  var upperCase = confirm("Would you like your pass word to have uppercase characters?")
+  if(upperCase){
+
+  // User selected "OK" to uppercase letters in psw...
+    criteriaList.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
+  }
+  // ALERT - Ask if user wants Password WITH Numbers
+  var numericCharacters = confirm("Would you like your password to have numbers?")
+  // User selected "OK" to numeric characters in psw...
+  if(numericCharacters){
+    criteriaList.push('0123456789Z'.split(''))
+  }
+    // ALERT - Ask if user wants Password WITH Special Characters
+  var specialCharacters = confirm("Even though you married one, would you like special characters in your password?")
+
+  // User selected "OK" to special characters in psw...
+  if(specialCharacters){
+    criteriaList.push('~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\'.split(''))
+  }
+// Verify if none of the options were selected
+  if(lowerCase===false && upperCase===false && numericCharacters===false && specialCharacters===false){
+    alert(" Be sure to select atleast one lowercase, uppercase, numeric and/or special characters.")
+  }
+}
+var flattenedArray = criteriaList.flat(1)
+// console.log(flattenedArray)
+// console.log( criteriaList)
+// console.log( criteriaList.length)
+
+var randompsw = []
+for(var i = 0; i <criteriaList.length; i++){
+  randompsw.push(criteriaList[i][Math.floor(Math.random() * criteriaList[i].length)])
+}
+
+var randoms = Array.from({length: pswLength-criteriaList.length}, () => Math.floor(Math.random() * flattenedArray.length));
+
+for(var i = 0; i<randoms.length; i++){
+  randompsw .push(flattenedArray[randoms[i]])
+}
+ return randompsw .join('');
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//
-
-// Various Arrays 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-// Variable Declaration 
-var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
-
-// Prompt to confirm how many characters the user would like in their password
-function generatePassword() {
-  var confirmLength = (prompt("How many characters would you like your password to contain? Please enter an number betwixt 8 - 128"));
-
-  // Loop if answer is outside the parameters 
-  while(confirmLength <= 7 || confirmLength >= 128) {
-      alert("Password length must be between 8-128 characters. Please try again.");
-      var confirmLength = (prompt("How many characters would you like your password to contain?"));
-      } 
-
-
-    // Determine criteria of password 
-    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-
-      // Loop if answer is outside the parameters 
-      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
-        alert("You must choose at least one parameter");
-        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters.");
-        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters.");    
-        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters.");
-        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters.");   
-    } 
-
-  }   
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # password-generator
 
-https://courses.bootcampspot.com/courses/744/files/885762/preview
